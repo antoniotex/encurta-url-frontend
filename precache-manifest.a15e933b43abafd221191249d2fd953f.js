cf8d00d632d27f92b1682ea6f3c622672586cb83
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-01 14:21:39 -0300

    Updates

---
+++ precache-manifest.a15e933b43abafd221191249d2fd953f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535145a790eca46e15939f685fa3c9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea4835d7ff9a70828482",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b402f3292b04b490ce26",
+    "url": "/static/css/main.20694494.chunk.css"
+  },
+  {
+    "revision": "ea4835d7ff9a70828482",
+    "url": "/static/js/2.1f6b3573.chunk.js"
+  },
+  {
+    "revision": "b402f3292b04b490ce26",
+    "url": "/static/js/main.c0db434a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
